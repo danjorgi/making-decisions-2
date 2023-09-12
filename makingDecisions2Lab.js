eca46ef6cbb1d378669569c203a9e6cce16d04d7
Daniel Jorgensen <dan.r.jorgensen@gmail.com>
2023-09-12 11:34:25 -0600

    Problem 11 Complete

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -151,7 +151,14 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
-
+// switch (letterGrade) {
+//   case A: console.log('The student is doing excellently.')
+//   case B: console.log('The student is doing well.')
+//   case C: console.log('The student is doing alright.')
+//   case D: console.log('The student is not doing very well.')
+//   case F: console.log('The student is failing.')
+//   default: console.log('Not an eligible grade.')
+// }
 
 
 ////////// PROBLEM 11 //////////
@@ -179,3 +186,14 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
+for (i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log('Devmountain')
+  } else if (i % 3 === 0) {
+    console.log('Dev')
+  } else if (i % 5 === 0) {
+    console.log('mountain')
+  } else {
+    console.log(i)
+  }
+}
