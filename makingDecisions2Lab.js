a46aa23c06f8f90a17bd791974510637d496bec1
Daniel Jorgensen <dan.r.jorgensen@gmail.com>
2023-09-12 10:39:02 -0600

    Problem 3 compelte

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -6,7 +6,6 @@ var arr = [10,20,30,40,50,60]
   Create a variable named 'firstItem' and set it equal to the first value of the 'arr' array.
 */
 
-//Code Here
 let firstItem = arr[0]
 
 ////////// PROBLEM 2 //////////
@@ -14,9 +13,9 @@ let firstItem = arr[0]
   Remove the last item from the 'arr' array and store it in a variable named 'lastItemRemoved'. Console.log the array to verify you no longer have the last item.
 */
 
-//Code Here
 let lastItemRemoved = arr.pop()
 console.log(arr)
+
 ////////// PROBLEM 3 //////////
 
 // Do not edit the code below.
@@ -28,8 +27,9 @@ var family = ['Tyler', 'Jordan', 'Ryan', 'Alice', 'Ireland'];
 */
 
 //Code Here
-
-
+for (i = 0; i < family.length; i++) {
+  console.log(family[i])
+}
 
 ////////// PROBLEM 4 //////////
 
